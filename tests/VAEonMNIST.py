54778e4ed776a918859495946b11225dd807bc72
MarioPaerle <paerle.music@gmail.com>
2025-03-22 11:18:34 +0100

    Starting

--- tests/VAEonMNIST.py
+++ tests/VAEonMNIST.py
@@ -146,3 +146,41 @@ def generate_digit(mean, var):
     plt.show()
 
 generate_digit(0.0, 1.0), generate_digit(1.0, 0.0)
+
+
+#######################################################################################################################
+#                                             Vediamo Lo Spazio Latente                                               #
+#######################################################################################################################
+
+
+def plot_latent_space(model, scale=1.0, n=25, digit_size=28, figsize=15):
+    # display a n*n 2D manifold of digits
+    figure = np.zeros((digit_size * n, digit_size * n))
+
+    # construct a grid
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = torch.tensor([[xi, yi]], dtype=torch.float).to(device)
+            x_decoded = model.decode(z_sample)
+            digit = x_decoded[0].detach().cpu().reshape(digit_size, digit_size)
+            figure[i * digit_size : (i + 1) * digit_size, j * digit_size : (j + 1) * digit_size,] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    plt.title('VAE Latent Space Visualization')
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("mean, z [0]")
+    plt.ylabel("var, z [1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+
+
+plot_latent_space(model)
